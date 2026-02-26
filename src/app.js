8847312eb05ce4038042203f9b2f5e4115608f6e
kjcfernandez <kelvin.cunalema@gmail.com>
2026-02-26 22:11:39 +0100

    Implement excuse generator functionality

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import "bootstrap";
-import "./style.css";
+let who = ['the dog','my granma','his turtle','my bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
 
+function generateExcuse() {
+  const randomWho = who[Math.floor(Math.random() * who.length)];
+  const randomAction = action[Math.floor(Math.random() * action.length)];
+  const randomWhat = what[Math.floor(Math.random() * what.length)];
+  const randomWhen = when[Math.floor(Math.random() * when.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload =function () {
+  document.getElementById('excuse').innerHTML = generateExcuse ();
 };
